2d227f8991dbb1e4074fdd4d159fda5c5b3b91d5
victor-mwania <victormwnza@gmail.com>
2018-03-02 21:58:48 +0300

    fixed the api

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () { 
     var regularStream=["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
     var output=[];
+    // channel ids
     for(var i=0; i<regularStream.length;i++){
     var url='https://api.twitch.tv/kraken/users/'+regularStream[i]+'?client_id=wm696nk23tnizjms0lxp737a4h900v';
     $.ajax({
@@ -9,11 +10,15 @@ $(document).ready(function () {
         dataType: "jsonp",
         jsonp:'callback',
         success: function (response) {
-           // console.log(response);
-            output+=response;
+            output.push(response);          
         }
     });
-  
 }
 console.log(output);
+// check if channel is online or offline by using ids from output array
+var ids=[];
+for(var j=0; j<output.length;j++){
+ids +=output[j];
+}
+console.log(ids);
 });
